14c4a738094cf8fbc0b793ee3ccba9178c01a610
gipeio <gipeio22@gmail.com>
2024-11-02 22:27:22 +0100

    gerer la responsivité

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -7,11 +7,6 @@ function Card({ card, onClick }) {
       <div className="card-image">
         <img src={card.imageUrl} alt={card.name} />
       </div>
-      <div className="card-details">
-        <h3>{card.name}</h3>
-        <p>{card.text || "No description available."}</p>
-        <p><strong>Artist:</strong> {card.artist}</p>
-      </div>
     </div>
   );
 }
